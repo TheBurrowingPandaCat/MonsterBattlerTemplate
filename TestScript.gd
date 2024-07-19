bfa9e5aac2ec7f131c94ea580fafa7f4dfc1e75a
TheBurrowingPandaCat <theburrowingpandacat@gmail.com>
2024-07-19 14:21:32 -0400

    Small input testing

--- TestScript.gd
+++ TestScript.gd
@@ -11,3 +11,12 @@ func _unhandled_input(event):
 		var mouse_event : InputEventMouseButton = event
 		if mouse_event.button_index == 1:
 			print("Left Click!")
+		elif mouse_event.button_index == 2:
+			print("Right Click!")
+	elif event is InputEventKey:
+		var key_event :InputEventKey = event
+		if key_event.keycode == KEY_SPACE:
+			if key_event.pressed:
+				print("Spacebar!")
+			else:
+				print("Spacebar Released!")
